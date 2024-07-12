dd092f5e6d961a8322f37d47d640d50c37dbe741
Anup Ray <rayanup95@gmail.com>
2024-07-12 18:33:43 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Body from "./components/Body";
+import Head from "./components/Head";
+function App() {
+  return (
+    <div>
+      <Head />
+      <Body />
+    </div>
+  );
+}
+
+/**
+ * Head
+ * Body
+ *    Sidebar
+ *        MenuItems
+ * MainContainer
+ *    ButtonList
+ *    VideoContainer
+ *        VideoCard
+ */
+
+export default App;
